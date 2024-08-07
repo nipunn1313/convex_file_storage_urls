36613583e15d7f8c763df911144dc13d635594cc
Nipunn Koorapati <nipunn1313@gmail.com>
2024-08-07 10:49:27 -0700

    Add tests

---
+++ file_storage_urls/serve.test.ts
@@ -0,0 +1,66 @@
+import { convexTest } from "convex-test";
+import { expect, test } from "vitest";
+import { functions } from "./_generated/api";
+import schema from "./schema";
+import { modules } from "./test.setup";
+import { MutationCtx } from "./_generated/server";
+
+test("Serve File", async () => {
+  const t = convexTest(schema, modules);
+  const storageId = await t.run(async (ctx) => {
+    return await ctx.storage.store(new Blob(["HI"]));
+  });
+  const uuid = await t.mutation(functions.serve.generateUrl, {
+    storageId,
+    expiresInMillis: null,
+  });
+  expect(uuid).toBeDefined();
+
+  const response = await t.fetch(`/get?uuid=${uuid}`, { method: "GET" });
+  expect(response.status).toEqual(200);
+  const contents = await response.text();
+  expect(contents).toEqual("HI");
+});
+
+test("Serve file expires", async () => {
+  const t = convexTest(schema, modules);
+  const storageId = await t.run(async (ctx) => {
+    return await ctx.storage.store(new Blob(["HI"]));
+  });
+  const uuid = await t.mutation(functions.serve.generateUrl, {
+    storageId,
+    expiresInMillis: 100,
+  });
+  expect(uuid).toBeDefined();
+
+  await new Promise((r) => setTimeout(r, 100));
+  const response = await t.fetch(`/get?uuid=${uuid}`, { method: "GET" });
+  expect(response.status).toEqual(404);
+  const contents = await response.text();
+  expect(contents).toEqual("File Not Found");
+});
+
+test("Cleanup", async () => {
+  const t = convexTest(schema, modules);
+  const storageId = await t.run(async (ctx) => {
+    return await ctx.storage.store(new Blob(["HI"]));
+  });
+
+  const getNumUrls = async (ctx: MutationCtx) => {
+    const urls = await ctx.db.query("urls").collect();
+    return urls.length;
+  };
+  expect(await t.run(getNumUrls)).toEqual(0);
+
+  const uuid = await t.mutation(functions.serve.generateUrl, {
+    storageId,
+    expiresInMillis: 100,
+  });
+  expect(uuid).toBeDefined();
+
+  expect(await t.run(getNumUrls)).toEqual(1);
+  await t.run(functions.serve.cleanup);
+
+  await new Promise((r) => setTimeout(r, 100));
+  expect(await t.run(getNumUrls)).toEqual(1);
+});
